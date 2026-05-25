cb5724853a820e1472e3e59d8c1240242ea49968
autovyn-anjali <Anjali.Sharma@autovyn.in>
2026-05-25 13:33:18 +0530

    repo

---
+++ src/main/java/com/validate/idvalidation/entity/ShTvEvl.java
@@ -0,0 +1,19 @@
+package com.validate.idvalidation.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "sh_tv_evl", schema = "muldms")
+@Getter
+@Setter
+public class ShTvEvl {
+
+    @Id
+    @Column(name = "buying_num")
+    private String buyingNum;
+}
--- src/main/java/com/validate/idvalidation/repository/MuldmsRepository.java
+++ src/main/java/com/validate/idvalidation/repository/MuldmsRepository.java
@@ -1,17 +1,19 @@
 package com.validate.idvalidation.repository;
 
+import com.validate.idvalidation.entity.ShTvEvl;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.Repository;
 import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
-public interface MuldmsRepository extends Repository<Object, Long> {
+public interface MuldmsRepository
+        extends JpaRepository<ShTvEvl, String> {
 
     @Query(value = """
-            SELECT BUYING_NUM
-            FROM MULDMS.SH_TV_EVL
-            WHERE BUYING_NUM IN (:buyingIds)
+            SELECT buying_num
+            FROM muldms.sh_tv_evl
+            WHERE buying_num IN (:buyingIds)
             """, nativeQuery = true)
     List<String> findExistingBuyingIds(
             @Param("buyingIds") List<String> buyingIds
